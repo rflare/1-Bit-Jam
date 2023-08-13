19e5f7a17792255816162454235b560ed211f1fb
rusluck <russellymallari@gmail.com>
2023-08-13 17:41:59 -0400

    a lot of commits

--- Jam Project/Assets/Player/Scripts/PlayerContext.cs
+++ Jam Project/Assets/Player/Scripts/PlayerContext.cs
@@ -1,12 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerContext : MonoBehaviour
 {
     //private properties
-    [SerializeField] private float speed;
+    [SerializeField] private float _speed;
+
+    [SerializeField] private float _debugLeft;
+    [SerializeField] private float _debugRight;
+
+    private PlayerControls _input;
     //Methods
+    void OnEnable()
+    {
+        _input.Enable();
+    }
+    void OnDisable()
+    {
+        _input.Disable();
+    }
+    void Awake()
+    {
+        _input = new PlayerControls();
+
+        _input.main.Left.performed += ctx => Debug.Log("hello");
+    }
     void Start()
     {
         
@@ -14,6 +34,11 @@ public class PlayerContext : MonoBehaviour
 
     void Update()
     {
-        
+        HandleMovement();
+    }
+    void HandleMovement()
+    {
+        _debugLeft = _input.main.Left.ReadValue<float>();
+        _debugRight = _input.main.Right.ReadValue<float>();
     }
 }
